8f634c0e986bbaa30b0b5077851a2dc75270ac52
Andrii Krenevych <krenevych@knu.ua>
2024-11-26 09:43:17 +0200

    ShopItemsRoomDatabase

---
+++ app/src/main/java/com/example/shoplist/data/db/ShopItemsRoomDatabase.kt
@@ -0,0 +1,36 @@
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.shoplist.data.db.DaoShopItem
+import com.example.shoplist.data.db.ShopEntity
+
+// Annotates class to be a Room Database with a table (entity) of the Word class
+@Database(entities = [ShopEntity::class], version = 1)
+public abstract class ShopItemsRoomDatabase : RoomDatabase() {
+
+    abstract fun wordDao(): DaoShopItem
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: ShopItemsRoomDatabase? = null
+
+        fun getDatabase(context: Context): ShopItemsRoomDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+//                if (INSTANCE != null) return@synchronized INSTANCE!!
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ShopItemsRoomDatabase::class.java,
+                    "shop_items_database"
+                ).build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}
